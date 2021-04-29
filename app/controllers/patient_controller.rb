90f052ce6ef545b0b569c631bd0c63b5551550da
sandeep <sandeeprajsaimon999@gmail.com>
2021-04-29 17:11:49 +0530

    normal route for get all patient

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -1,2 +1,11 @@
+require_relative "../utils/response.rb"
 class PatientController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  include Response
+
+  def getAll
+    msg = Response.customSuccessResponse("SUCCESS",nil)
+    render :json=>msg
+  end
+
 end
